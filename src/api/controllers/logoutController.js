072af37d2d33f4285e9470af44a33ad684964d82
Ala075 <ala.arfaoui751@gmail.com>
2023-12-03 14:43:34 +0100

    Initial Commit

--- src/api/controllers/logoutController.js
+++ src/api/controllers/logoutController.js
@@ -1,6 +1,8 @@
 
 
 const goOut = async (req, res) => {
+    res.cookie("jwt","",{ maxAge: 1 });
+    res.redirect("/");
     res.status(200).json({sucess: true, message: "logout sccessfully"});
 };
 
